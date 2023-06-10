e7ad602a8cfd7f29add9337707fd18f213b5232c
jhg <kospao7142@gmail.com>
2023-06-10 16:05:39 +0900

    20230610

--- lib/oop.dart
+++ lib/oop.dart
@@ -1,22 +1,32 @@
-class Human {
-  final String name;
-  Human(this.name);
-  void sayHello() {
-    print("Hi my name is $name");
+mixin Strong {
+  final double strengthLevel = 1500.99;
+}
+
+mixin QuickRunner {
+  void runQuick() {
+    print("ruuuuuun!");
   }
+}
+
+mixin Tall {
+  final double height = 1.99;
 }
 
 enum Team { blue, red }
 
-class Player extends Human {
+class Player with Strong, QuickRunner, Tall {
   final Team team;
 
-  Player({
-    required this.team,
-    required String name
-  })
+  Player({required this.team});
 }
 
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
 void main() {
-  var player = Player(team: Team.red);
+  var player = Player(
+    team: Team.red,
+  );
+  player.runQuick();
 }
